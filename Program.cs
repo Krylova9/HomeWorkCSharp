1caacb2156149314089167896c159764de2a9004
Ксения Крылова <kseniakrylova@MacBook-Air-Ksenia.local>
2023-03-12 18:33:07 +0700

    lesson1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите свое имя:");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Привет {name}");
+        }
+    }
+}
