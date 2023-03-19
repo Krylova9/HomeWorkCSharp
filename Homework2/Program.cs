68a8fb9f86d20318b7dc72aeed4512e0bf09b62b
Ксения Крылова <kseniakrylova@MacBook-Air-Ksenia.local>
2023-03-19 23:34:49 +0700

    Homework2

---
+++ Homework2/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Homework2;
+
+public static class Program
+{
+    public static void Main()
+    {
+        try 
+        {
+            Console.WriteLine("Введите размер массива:");
+            int n = ReadInt();
+            int[] array = new int[n];
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"Введите номер {i + 1} массива: ");
+                array[i] = ReadInt();
+            }
+            int[] arrayTop = new int[2];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if(array[i] > arrayTop[0])
+                {
+                    arrayTop[1] = arrayTop[0];
+                    arrayTop[0] = array[i];
+                } else if (array[i] > arrayTop[1])
+                {
+                    arrayTop[1] = array[i];
+                }
+            }
+            Console.WriteLine($"Вторым наибольшим числом является {arrayTop[1]}");
+        }
+        catch (Exception){}
+    }
+    public static int ReadInt()
+    {
+            if (Int32.TryParse(Console.ReadLine(), out var value))
+            {
+                return value;
+            }
+            else
+            {
+                throw new Exception("укажите размер массива в виде числа");
+            }
+    }
+    
+}
